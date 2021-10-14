8430070fa549c514d4da354d851414310953f4ea
gorazdnina <ninagorazd@gmail.com>
2021-10-14 13:43:57 +0200

    Added controller

---
+++ src/main/java/com/Lab1/lab1/controller/StringController.java
@@ -0,0 +1,19 @@
+package com.Lab1.lab1.controller;
+
+import com.Lab1.lab1.dto.ReverseRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StringController {
+
+    @GetMapping("/request")
+    public ResponseEntity<String> postController(@RequestBody ReverseRequest reverseRequest) {
+        String toReverse = reverseRequest.getString();
+        String reversedString = new StringBuilder(toReverse).reverse().toString();
+        return ResponseEntity.ok(reversedString);
+    }
+}
