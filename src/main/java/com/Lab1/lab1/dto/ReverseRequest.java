81e6ffddf91b1f3608ae150f66a715ef10dc4dd8
gorazdnina <ninagorazd@gmail.com>
2021-10-14 13:09:27 +0200

    Added dto package

---
+++ src/main/java/com/Lab1/lab1/dto/ReverseRequest.java
@@ -0,0 +1,12 @@
+package com.Lab1.lab1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReverseRequest {
+    private String string;
+}
