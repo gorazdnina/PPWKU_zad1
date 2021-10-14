a3458dde44b7e8cc4ba8f81171c3295feca5c4a4
gorazdnina <ninagorazd@gmail.com>
2021-10-14 13:47:40 +0200

    Remove unused PostMapping

--- src/main/java/com/Lab1/lab1/controller/StringController.java
+++ src/main/java/com/Lab1/lab1/controller/StringController.java
@@ -3,7 +3,6 @@ package com.Lab1.lab1.controller;
 import com.Lab1.lab1.dto.ReverseRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StringController {
 
     @GetMapping("/request")
-    public ResponseEntity<String> postController(@RequestBody ReverseRequest reverseRequest) {
+    public ResponseEntity<String> getController(@RequestBody ReverseRequest reverseRequest) {
         String toReverse = reverseRequest.getString();
         String reversedString = new StringBuilder(toReverse).reverse().toString();
         return ResponseEntity.ok(reversedString);
